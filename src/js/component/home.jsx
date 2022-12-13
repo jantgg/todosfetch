a40dc38e7620b769c342045957b347cc0cc118e6
jantgg <smosh360@hotmail.com>
2022-12-13 11:55:33 +0000

    todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row d-flex flex-column">
+			<div className="col-3 mx-auto mt-5"><strong>To Dos</strong></div>
+			<input className="col-3 mx-auto mt-2" onKeyUp={(e)=>{
+				if(e.key =="Enter"){
+					let newTodos = e.target.value;
+					setTodos([...todos, newTodos]); 
+				}
+			}}></input>
+            {todos.map((todo, index)=>{
+				return (
+					<div key={index} className="col-3 mx-auto mt-1 bg-light d-flex justify-content-between align-items-center">
+                    <div>{todo}</div>
+					<button type="button" class="btn btn-danger" onClick={()=>{
+					delete todos[{index}]}}><i class="fa-solid fa-trash-can"></i></button>
+					</div>
+				)
+			})};
+			<div >
+  
+ 
+			</div>
+
 		</div>
+	
 	);
 };
 
