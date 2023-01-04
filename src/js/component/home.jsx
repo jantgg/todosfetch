c99e90ea2f4a7bc590642cbef23cbee1424f8bde
jantgg <smosh360@hotmail.com>
2023-01-04 15:46:21 +0000

    primerpush

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,51 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState({"label":"","done":false});
 	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		getTodos();
+	  }, []);
+	
+	  const getTodos = async () => {
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jantgg" );
+		const data = await response.json();setTodos(data);};
+	
+	  const updateTodos = async () => {
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jantgg" , {
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(
+			 todos.concat([inputValue]))});
+		if (response.ok) {getTodos()}};
+
 
 	return (
 		<div className="row d-flex flex-column">
 			<div className="col-3 mx-auto mt-5"><strong>To Dos</strong></div>
-			<input className="col-3 mx-auto mt-2" placeholder="Add To Do" onKeyUp={(e)=>{
+			<input className="col-3 mx-auto mt-2" 
+				placeholder="Add To Do" 
+				value={inputValue.label}
+				onChange={(e) => setInputValue({"label":e.target.value,"done":false})}
+				onKeyUp={(e)=>{
 				if(e.key =="Enter" && e.target.value.trim() !=""){
-					setTodos([...todos, e.target.value]); 
-					e.target.value="";
+					updateTodos()
 				}
 			}}></input>
             {todos.map((todo, index)=>{
 				return (
 					<div key={index} className="col-3 mx-auto mt-1 bg-light d-flex justify-content-between align-items-center">
-                    <div>{todo}</div>
+                    <div>{todo.label}</div>
 					<button type="button" className="btn btn-danger" onClick={()=>{
-						let newTodos = [];
-						for(let x=0; x<todos.length; x++){
-							if(x!=index){
-								newTodos.push(todos[x])
-							}
-						}
-						setTodos(newTodos);
+						 setTodos(
+							todos.filter((t, currentIndex) => index != currentIndex)
+						  )
+						
 					
 					}}><i className="fa-solid fa-trash-can"></i></button>
 					</div>
