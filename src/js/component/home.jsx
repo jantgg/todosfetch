e6446c5ade7619640d51837670583b43f619629c
jantgg <smosh360@hotmail.com>
2023-01-04 11:16:27 +0000

    correcion2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,7 +21,13 @@ const Home = () => {
 					<div key={index} className="col-3 mx-auto mt-1 bg-light d-flex justify-content-between align-items-center">
                     <div>{todo}</div>
 					<button type="button" className="btn btn-danger" onClick={()=>{
-						setTodos(todos.filter((e, i)=>e != todo))
+						let newTodos = [];
+						for(let x=0; x<todos.length; x++){
+							if(x!=index){
+								newTodos.push(todos[x])
+							}
+						}
+						setTodos(newTodos);
 					
 					}}><i className="fa-solid fa-trash-can"></i></button>
 					</div>
